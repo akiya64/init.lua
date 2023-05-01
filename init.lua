9434d628f3b5d932c995bd7597dee47bd71865d9
akiya <autumnsky64@gmail.com>
2023-05-01 19:05:54 +0900

    Add comment setting plan

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
 require('colemak')
+-- keymap / leader
+--
 require('options')
+
 require('plugins')
 require('lsp')
 require('status_line')
+-- filer
+-- session
+-- git command
 
 vim.cmd [[
 colorscheme soifon

e9132696aa2f8df9c00d42cb24f8e85a210864f7
akiya <autumnsky64@gmail.com>
2023-05-03 19:30:04 +0900

    Setting autopairs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,7 +18,10 @@ require('packer').startup(function()
   use 'lukas-reineke/indent-blankline.nvim'
 
   use 'Editorconfig/editorconfig-vim'
-  use 'windwp/nvim-autopairs'
+  use {
+		'windwp/nvim-autopairs',
+		config = function() require("nvim-autopairs").setup {} end
+}
 
   -- lsp
   use 'neovim/nvim-lspconfig'

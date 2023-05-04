847bef56ac740db031a10d5332d117fca5076ca3
akiya <autumnsky64@gmail.com>
2023-05-04 10:37:48 +0900

    Set linter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,16 +5,18 @@ require('packer').startup(function()
 	use('nvim-lua/popup.nvim')
 	use('nvim-telescope/telescope.nvim')
 
-	use('Shougo/defx.nvim')
-	use('Shougo/denite.nvim')
-
 	use('nvim-lualine/lualine.nvim')
-
-	use('nvim-tree/nvim-web-devicons')
 
 	use('akiya64/soifon')
 
-	use('lewis6991/gitsigns.nvim')
+	use('nvim-tree/nvim-web-devicons')
+	use({
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require('gitsigns').setup()
+		end,
+	})
+
 	use('lukas-reineke/indent-blankline.nvim')
 
 	use('Editorconfig/editorconfig-vim')
@@ -22,11 +24,13 @@ require('packer').startup(function()
 	use({
 		'windwp/nvim-autopairs',
 		config = function()
-			require('nvim-autopairs').setup({})
+			require('nvim-autopairs').setup()
 		end,
 	})
+
 	-- formatter
 	use('wesleimp/stylua.nvim')
+
 	-- lint
 	use('mfussenegger/nvim-lint')
 

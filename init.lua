6edee7b92fcd4c5f227cdcd191b364cb6c0f6b08
akiya <autumnsky64@gmail.com>
2023-05-08 11:40:00 +0900

    Add indentline highlight

--- init.lua
+++ init.lua
@@ -25,3 +25,9 @@ vim.api.nvim_create_autocmd('QuickFixCmdPost',{
 	pattern = '*grep*',
 	command = 'cwindow'
 })
+vim.cmd [[highlight IndentBlanklineIndent guifg=#c0c0c0 gui=nocombine ]]
+vim.cmd [[highlight IndentBlanklineContextchar guifg=#909090 gui=nocombine ]]
+require("indent_blankline").setup {
+	show_current_context = true,
+	char_highlight_list = { "IndentBlanklineIndent" },
+}

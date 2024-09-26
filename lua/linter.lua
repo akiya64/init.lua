db8e2674a929d7a89390b9ac4644f37bd8f1d0c6
Akiya <autumnsky64@gmail.com>
2024-09-26 13:43:28 +0900

    Fix for remove lsp.buf.server_ready

--- lua/linter.lua
+++ lua/linter.lua
@@ -8,14 +8,15 @@ require('lint').linters_by_ft = {
 
 vim.diagnostic.config({virtual_text = false, float={source=true},signs=false})
 
-local function on_cursor_hold()
-  if vim.lsp.buf.server_ready() then
-    vim.diagnostic.open_float()
-  end
-end
-
 local diagnostic_hover_augroup_name = "lspconfig-diagnostic"
 vim.api.nvim_set_option('updatetime', 500)
 vim.api.nvim_create_augroup(diagnostic_hover_augroup_name, { clear = true })
-vim.api.nvim_create_autocmd({ "CursorHold" }, { group = diagnostic_hover_augroup_name, callback = on_cursor_hold })
-
+vim.api.nvim_create_autocmd(
+	{ "CursorHold" },
+	{
+		group = diagnostic_hover_augroup_name,
+		callback = function()
+			vim.diagnostic.open_float()
+		end
+	}
+)

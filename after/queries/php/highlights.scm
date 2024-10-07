b55fa95c3ede35f70d00b351be8a3b1a3c38d225
Akiya <autumnsky64@gmail.com>
2024-10-07 16:43:02 +0900

    Add operator in if statement

--- after/queries/php/highlights.scm
+++ after/queries/php/highlights.scm
@@ -1,5 +1,22 @@
 ;; extends
-(
-  (function_definition
-    name: (name) @string )
+(function_definition
+  name: (name) @string )
+
+(binary_expression
+  operator: [
+  "=>"
+  "<"
+  "<="
+  ">="
+  ">"
+  "<>"
+  "<=>"
+  "=="
+  "!="
+  "==="
+  "!=="
+  "!"
+  "&&"
+  "||"
+    ] @punctuation.bracket 
 )

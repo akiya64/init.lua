a820f505444890ec9d574633ad445a11ab1111eb
Akiya <autumnsky64@gmail.com>
2026-02-18 14:55:45 +0900

    Add option for local llm

--- lua/plugins/ai-agent.lua
+++ lua/plugins/ai-agent.lua
@@ -21,8 +21,41 @@ return {
     },
     opts = function(_, opts)
       local base_opts = {
-        languages = "Japanese"
+        languages = "Japanese",
+        strategies = {
+            chat = {
+              adapter = function()
+                return require('codecompanion.adapters').extend('ollama', {
+                  schema = {
+                    model = {
+                      default = 'granite3.3:8b'
+                    },
+                  },
+                })
+              end,
+               tools = {
+                    ["mcp"] = { -- Name this tool whatever you like
+                        -- Callback provides the necessary functions to CodeCompanion
+                        callback = function()
+                            return require("mcphub.extensions.codecompanion")
+                        end,
+                        opts = {
+                          -- If true, CodeCompanion will ask for approval before executing the MCP tool call
+                          requires_approval = false,
+                          -- Optional: Pass parameters like temperature to the underlying LLM if the chat strategy supports it
+                          temperature = 0.7
+                        }
+                    }
+                    -- Add other CodeCompanion tools here
+                }
+            }
+            -- Other CodeCompanion strategies
+        },
+        opts = {
+          log_level = 'DEBUG',
+        },
       }
+
       local env_opts = require("envs.code-companion").opts
 
       return vim.tbl_deep_extend("force", opts, base_opts, env_opts)
